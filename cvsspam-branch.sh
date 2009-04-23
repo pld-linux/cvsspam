96c01d17c29070fb8c3e12cc4f433a280e6601f5
Elan Ruusamäe <glen@pld-linux.org>
2009-04-23 17:20:35 +0000

    - svn support merged to trunk
    Changed files:
        cvsspam-branch.diff -> 1.7
        cvsspam-branch.sh -> 1.2

--- cvsspam-branch.sh
+++ cvsspam-branch.sh
@@ -3,6 +3,4 @@ svn="http://svn.badgers-in-foil.co.uk/cvsspam"
 tag=RELEASE-0_2_12
 
 svn diff --old=$svn/tags/$tag --new=$svn/trunk | filterdiff -x project.xml > cvsspam-branch.diff
-
-branch=svn_support
-svn diff --old=$svn/trunk --new=$svn/branches/$branch | filterdiff -i svn_post_commit_hook.rb -i cvsspam.rb > cvsspam-svnspam-branch.diff
+sed -i -e 's,\$''Revision\$,$''Revision: 1.12 $,' cvsspam-branch.diff
